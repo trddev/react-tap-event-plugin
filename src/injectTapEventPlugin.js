24acdad652fa943680278c17f6a3aeae52c25546
Chris Kee <ckee83@gmail.com>
2017-07-05 12:29:25 -0700

    reverted es6 class implementation

--- src/injectTapEventPlugin.js
+++ src/injectTapEventPlugin.js
@@ -1,25 +1,17 @@
 var invariant = require('fbjs/lib/invariant');
 var defaultClickRejectionStrategy = require('./defaultClickRejectionStrategy');
 
-class Injector {
-  constructor() {
-    this.alreadyInjected = false;
+var alreadyInjected = false;
+
+module.exports = function injectTapEventPlugin (strategyOverrides) {
+  strategyOverrides = strategyOverrides || {}
+  var shouldRejectClick = strategyOverrides.shouldRejectClick || defaultClickRejectionStrategy;
+
+  if (!alreadyInjected) {
+    alreadyInjected = true;
+
+    require('react-dom/lib/EventPluginHub').injection.injectEventPluginsByName({
+      'TapEventPlugin':       require('./TapEventPlugin.js')(shouldRejectClick)
+    });
   }
-
-  injectTapEventPlugin (strategyOverrides) {
-    console.log('USING FORKED INJECTOR');
-    const self = this;
-    strategyOverrides = strategyOverrides || {}
-    var shouldRejectClick = strategyOverrides.shouldRejectClick || defaultClickRejectionStrategy;
-
-    if (!self.alreadyInjected) {
-      self.alreadyInjected = true;
-      require('react-dom/lib/EventPluginHub').injection.injectEventPluginsByName({
-        'TapEventPlugin': require('./TapEventPlugin.js')(shouldRejectClick)
-      });
-    }
-  }
-}
-const injector = new Injector();
-
-module.exports = injector.injectTapEventPlugin.bind(injector);
+};
