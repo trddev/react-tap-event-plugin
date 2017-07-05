fc531189ea287b28fed2769d41adc00b038f0af1
Chris Kee <ckee83@gmail.com>
2017-07-05 12:17:55 -0700

    refactored injectTapEventPlugin as singleton

--- src/injectTapEventPlugin.js
+++ src/injectTapEventPlugin.js
@@ -1,26 +1,27 @@
 var invariant = require('fbjs/lib/invariant');
 var defaultClickRejectionStrategy = require('./defaultClickRejectionStrategy');
 
-var alreadyInjected = false;
-
-module.exports = function injectTapEventPlugin (strategyOverrides) {
-  strategyOverrides = strategyOverrides || {}
-  var shouldRejectClick = strategyOverrides.shouldRejectClick || defaultClickRejectionStrategy;
-
-  if (process.env.NODE_ENV !== 'production') {
-    invariant(
-      !alreadyInjected,
-      'injectTapEventPlugin(): Can only be called once per application lifecycle.\n\n\
-It is recommended to call injectTapEventPlugin() just before you call \
-ReactDOM.render(). If you are using an external library which calls injectTapEventPlugin() \
-itself, please contact the maintainer as it shouldn\'t be called in library code and \
-should be injected by the application.'
-    )
+class Injector {
+  constructor() {
+    this.alreadyInjected = false;
   }
 
-  alreadyInjected = true;
+  injectTapEventPlugin (strategyOverrides) {
+    console.log('USING FORKED INJECTOR');
+    const self = this;
+    strategyOverrides = strategyOverrides || {}
+    var shouldRejectClick = strategyOverrides.shouldRejectClick || defaultClickRejectionStrategy;
 
-  require('react-dom/lib/EventPluginHub').injection.injectEventPluginsByName({
-    'TapEventPlugin':       require('./TapEventPlugin.js')(shouldRejectClick)
-  });
+    if (!self.alreadyInjected) {
+      self.alreadyInjected = true;
+      require('react-dom/lib/EventPluginHub').injection.injectEventPluginsByName({
+        'TapEventPlugin': require('./TapEventPlugin.js')(shouldRejectClick)
+      });
+    }
+  }
+}
+const injector = new Injector();
+
+module.exports = {
+  injectTapEventPlugin: injector.injectTapEventPlugin.bind(injector),
 };
